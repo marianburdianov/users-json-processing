f86d7ee55ec7ef89c8e7edff1974c650896c2c01
marianburdianov <marian.burdianov@gmail.com>
2022-05-05 22:42:19 +0300

    first commit

---
+++ src/main/java/com/example/usersjsonprocessing/UsersJsonProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.usersjsonprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersJsonProcessingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersJsonProcessingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/usersjsonprocessing/UsersJsonProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usersjsonprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersJsonProcessingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
